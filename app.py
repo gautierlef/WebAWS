287987cbf58689366a24a47d585b2bbdef7ec1fc
gautierlef <gautier.leffebvre.sio@gmail.com>
2022-03-06 16:47:09 +0100

    Add menus

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import mysql.connector
 
 
@@ -15,11 +15,19 @@ def ajoutMatiereMenu():
     return render_template('vueAjoutMatiere.html')
 
 
+@app.route('/ajoutMatiereMenu/<idMatiere>', methods=['GET'])
+def modifierMatiereMenu(idMatiere):
+    storage = Storage()
+    data = storage.loadMatiere(idMatiere)
+    matiere = {'id': str(data[0][0]), 'nom': data[0][1], 'description': data[0][2], 'heures': str(data[0][3])}
+    return render_template('vueModifierMatiere.html', matiere=matiere)
+
+
 @app.route('/voirMatieres', methods=['GET'])
 def voirMatieres():
     matieres = []
     storage = Storage()
-    data = storage.load()
+    data = storage.loadAll()
     print(data)
     for row in data:
         matieres.append({'id': str(row[0]), 'nom': row[1], 'description': row[2], 'heures': str(row[3])})
@@ -34,6 +42,21 @@ def ajoutMatiere():
     storage.addMatiere(matiere)
     return render_template('vueAccueil.html')
 
+@app.route('/modifierMatiere', methods=['POST'])
+def modifierMatiere():
+    data = request.form
+    matiere = {'nom': data['nom'], 'description': data['description'], 'heures': int(data['heures'])}
+    storage = Storage()
+    storage.updateMatiere(matiere)
+    return render_template('vueAccueil.html')
+
+
+@app.route('/supprimerMatiere/<idMatiere>', methods=['GET'])
+def supprimerMatiere(idMatiere):
+    storage = Storage()
+    storage.deleteMatiere(idMatiere)
+    return redirect(url_for('voirMatieres'))
+
 
 class Storage:
     def __init__(self):
@@ -45,15 +68,33 @@ class Storage:
             port=3306
         )
 
-    def load(self):
+    def loadAll(self):
         cur = self.db.cursor()
         cur.execute(''' SELECT id, nom, description, heures FROM Matieres ''')
         data = cur.fetchall()
         return data
 
+    def loadMatiere(self, idMatiere):
+        cur = self.db.cursor()
+        cur.execute(''' SELECT id, nom, description, heures FROM Matieres WHERE id = %s ''', idMatiere)
+        matiere = cur.fetchall()
+        return matiere
+
     def addMatiere(self, matiere):
         cur = self.db.cursor()
         cur.execute(''' INSERT INTO Matieres(nom, description, heures) VALUES (%s, %s, %s) '''
+                    , (matiere['nom'], matiere['description'], matiere['heures']))
+        self.db.commit()
+
+    def deleteMatiere(self, idMatiere):
+        cur = self.db.cursor()
+        cur.execute(''' DELETE FROM Matieres WHERE id = %s) '''
+                    , idMatiere)
+        self.db.commit()
+
+    def updateMatiere(self, matiere):
+        cur = self.db.cursor()
+        cur.execute(''' UPDATE Matieres SET nom = %s, description = %s, heures = %s WHERE id = %s) '''
                     , (matiere['nom'], matiere['description'], matiere['heures']))
         self.db.commit()
 
